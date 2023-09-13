b16ee6a8ce03d6af3141d29808e55b6a598c3f7d
JonathanAL003 <jonathan_alonsolara@yahoo.com>
2023-09-13 08:48:49 -0500

    Delete en ServerBd

--- Primer Parcial/ServerBD/index.js
+++ Primer Parcial/ServerBD/index.js
@@ -63,37 +63,18 @@ app.get("/producto/:id", (req, res) => {
 });
 
 
-
-// app.get("/producto",(req,res)=>{
-//   mysql.createConnection({host:'localhost',user:'root',password:'',database:'bdweb'})
-//   .then(conn => conn.query('SELECT * from producto'))
-//   .then(([rows, fields]) => res.json(rows));
-// })
-
-// app.get("/producto/:id",(req,res)=>{
-//   console.log(req.params.id)
-//   mysql.createConnection({host:'localhost',user:'root',password:'',database:'bdweb'})
-//   .then(conn => conn.query('SELECT * from producto where Id_Producto='+req.params.id))
-//   .then(([rows, fields]) => res.json(rows));
-// })
-
-// app.get("/productos",async (req,res)=>{
-//   try{
-//   const conn = await mysql.createConnection({host:'localhost',user:'root',password:'',database:'bdweb'})
-//   const [rows,fields] = await conn.query('SELECT * from producto');
-//   res.json(rows);
-//   } catch{
-//     res.json({mensaje: "Error de conexion"});
-//   }
-//   });
-
-// app.get("/productos/:id",async (req,res)=>{
-//   console.log(requ.params.id);
-//   const conn = await mysql.createConnection({host:'localhost',user:'root',password:'',database:'bdweb'})
-//   const [rows,fields] = await conn.query('SELECT * from producto where Id_Producto='+req.params.id)
-//   if(rows.length==0){
-//     res.json({mensaje: "No existe el producto"})
-//   } else {
-//     res.json(rows);
-//   }
-// });
+app.delete("/producto/:id", (req, res) => {
+    const productId = req.params.id;
+    console.log(req.params.id);
+    connection.query('DELETE FROM producto WHERE Id_Producto = ?', [productId], (error, results) => {
+        if (error) {
+            res.status(500).json({ mensaje: "Error de base de datos" });
+        } else {
+            if (results.length === 0) {
+                res.status(404).json({ mensaje: "No existe el producto" });
+            } else {
+                res.json({mensaje : "Registro eliminado con exito"});
+            }
+        }
+    });
+  });

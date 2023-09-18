e71af7f7a766482914830fe4692047bfb0493aa3
JonathanAL003 <jonathan_alonsolara@yahoo.com>
2023-09-18 08:31:48 -0500

    BD Tabla Api

--- Primer Parcial/ServerBD/index.js
+++ Primer Parcial/ServerBD/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
 const mysql = require('mysql2')
+app.use(cors());
 app.use(express.json());
 const promisePool = require('./connection.js');
 

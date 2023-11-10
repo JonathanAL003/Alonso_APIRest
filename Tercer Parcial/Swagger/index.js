6c8f67af656ba42abd66d850b0da16b526d4abce
JonathanAL003 <jonathan_alonsolara@yahoo.com>
2023-11-10 11:32:56 -0600

    Swagger Ruta V.1.0.2

--- Tercer Parcial/Swagger/index.js
+++ Tercer Parcial/Swagger/index.js
@@ -1,16 +1,17 @@
 const express = require('express');
+const app = express();
 const path = require('path');
 const cors = require('cors');
+const mysql = require('mysql2');
 const swaggerUI = require('swagger-ui-express');
 const swaggerJsdoc = require('swagger-jsdoc');
-const productoRoutes = require('./routes/productoRoutes');
 
-const app = express();
 const PORT = 8080;
 
 app.use(cors());
 app.use(express.json());
 
+const productoRoutes = require('./routes/productoRoutes');
 const swaggerOptions = {
   definition: {
     openapi: '3.0.0',
@@ -19,7 +20,9 @@ const swaggerOptions = {
       version: '1.0.0',
     },
   },
-  apis: [`${path.join(__dirname,"./routes/productoRoutes.js")}`]
+  apis: [
+    `${path.join(__dirname, "./routes/productoRoutes.js")}`,
+  ],
 };
 
 const swaggerDocs = swaggerJsdoc(swaggerOptions);

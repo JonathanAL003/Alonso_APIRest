589f9e341d01812cc76b0bbfc043fad8ed99dca8
JonathanAL003 <jonathan_alonsolara@yahoo.com>
2023-11-14 09:12:11 -0600

    Swagger Ruta V. 1.0.6 Readme y Redoc Express

--- Tercer Parcial/Swagger/index.js
+++ Tercer Parcial/Swagger/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const redoc = require('redoc-express');
 const app = express();
 const path = require('path');
 const fs =require('fs');
@@ -13,7 +14,10 @@ app.use(cors());
 app.use(express.json());
 
 const def = fs.readFileSync(path.join(__dirname,"./swaggerOptions.json"), {encoding: "utf8", flag:"r"});
+const readmeContent = fs.readFileSync(path.join(__dirname, './README.md'), { encoding: 'utf8', flag: 'r' });
+
 const defObj =JSON.parse(def);
+defObj.info.description = readmeContent;
 const swaggerOptions = {
   definition: defObj,
   apis : [path.join(__dirname,"./routes/productoRoutes.js")]
@@ -38,6 +42,36 @@ app.use('/api-docs-json', (req,res) => {
 app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
+
+app.get(
+  '/docs',
+  redoc({
+    title: 'API Docs',
+    specUrl: '/api-docs-json', // Use the correct path for your swagger.json
+    routePrefix: '/docs', // Optional, defaults to '/docs'
+    options: {
+      theme: {
+        colors: {
+          primary: {
+            main: '#6EC5AB'
+          }
+        },
+        typography: {
+          fontFamily: `"museo-sans", 'Helvetica Neue', Helvetica, Arial, sans-serif`,
+          fontSize: '15px',
+          lineHeight: '1.5',
+          code: {
+            code: '#87E8C7',
+            backgroundColor: '#4D4D4E'
+          }
+        },
+        menu: {
+          backgroundColor: '#ffffff'
+        }
+      }
+    }
+  })
+);
 
 app.listen(PORT, () => {
   console.log(`Servidor express escuchando en el puerto ${PORT}`);
